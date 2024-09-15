01188ec25aff753fc392178af555a890166d7158
josepinheiro89 <66703840+josepinheiro89@users.noreply.github.com>
2024-09-15 17:53:06 -0300

    Add files via upload

--- payment_scheduler/schedules/models.py
+++ payment_scheduler/schedules/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+from decimal import Decimal
 
 class PaymentSchedule(models.Model):
     data_pagamento = models.DateField()
     permite_recorrencia = models.BooleanField(default=False)
-    quantidade_recorrencia = models.IntegerField()
-    intervalo_recorrencia = models.IntegerField()
+    quantidade_recorrencia = models.IntegerField(default=0)
+    intervalo_recorrencia = models.IntegerField(default=0)
     status_recorrencia = models.CharField(max_length=20)
     agencia = models.IntegerField()
     conta = models.IntegerField()
-    valor_pagamento = models.IntegerField()  # Salvo como inteiro
+    valor_pagamento = models.FloatField()  # Salvo como inteiro
+
 
     def save(self, *args, **kwargs):
-        # Converte o valor de decimal para inteiro
-        self.valor_pagamento = int(self.valor_pagamento)
+        # Verifica se o valor é decimal e converte para inteiro
+        if isinstance(self.valor_pagamento, float) or isinstance(self.valor_pagamento, Decimal):
+            self.valor_pagamento = int(self.valor_pagamento)
         super().save(*args, **kwargs)
+
